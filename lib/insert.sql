5e9cdd5644552d946a838d11a07da89e32ae7c22
Miwa <paintfx@gmail.com>
2018-01-07 14:44:21 +0000

    automatically backed up by learn

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 4, "M", "brown", "aggresive", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 6, "M", "black", "calm", 1);
+INSERT INTO bears VALUES (3,"Tabitha", 2, "F", "brown", "mellow", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 1, "M", "brown", "mellow", 1);
+INSERT INTO bears VALUES (5,"Melissa", 6, "F", "brown", "goofy", 1);
+INSERT INTO bears VALUES (6,"Grinch", 4, "M", "brown", "mellow", 1);
+INSERT INTO bears VALUES (7,"Wendy", 2, "F", "black", "mellow", 1);
+INSERT INTO bears VALUES (8,null, 20, "M", "black", "aggressive", 0);
